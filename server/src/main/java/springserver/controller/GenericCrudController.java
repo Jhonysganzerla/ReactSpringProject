abc8fa8b77412b53329724851847fc2821776538
Jhony <Jhonysganzerla@hotmail.com>
2022-05-04 22:20:25 -0300

    Melhorias Restful

--- server/src/main/java/springserver/controller/GenericCrudController.java
+++ server/src/main/java/springserver/controller/GenericCrudController.java
@@ -1,7 +1,6 @@
 package springserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.*;
 import springserver.service.GenericCrudService;
 
@@ -16,23 +15,23 @@ public class GenericCrudController<T,ID extends Serializable> {
     @Autowired
     private GenericCrudService<T,ID> genericCrudService;
 
-    @GetMapping("/")
+    @GetMapping
     public List<T> getAll() {
         return genericCrudService.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Optional<T> getById(ID id) {
+    @GetMapping("{id}")
+    public Optional<T> findOne(@PathVariable ID id) {
         return genericCrudService.findOne(id);
     }
 
-    @PostMapping("/save")
+    @PostMapping
     public T save(@Valid @RequestBody T t) {
         return genericCrudService.save(t);
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(ID id) {
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable ID id) {
         genericCrudService.delete(id);
     }
 
